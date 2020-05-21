95e8240c3519965c0ac59b543d9c4e2bc7772c26
lakiseven <fede.pazos@hotmail.com>
2020-05-21 11:26:25 -0300

    1 file modified

--- src/app/card-two/card-two.component.ts
+++ src/app/card-two/card-two.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';@Component({
   selector: 'app-card-two',
   template: `
     <div class="card"></div>
+    
     <style>
       .card {
-        background-image: url(../../assets/glasses.gif);                              
+        background-image: url('https://media1.giphy.com/media/3oKHWE2vSaeAJfgU1i/200.webp?cid=ecf05e4738add8a35816894e3a12559de9c1981545e210f0&rid=200.webp');           
+        background-color: #66ff99;                   
         background-size: cover;
         box-shadow: 0 10px 20px rgba(0,0,0,0.19), 
                     0 6px 6px rgba(0,0,0,0.23);
